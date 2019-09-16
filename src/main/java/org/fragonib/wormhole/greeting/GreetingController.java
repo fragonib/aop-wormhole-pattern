45c6be8e233ee482a0ce664a8707997744651b01
Francisco González Ibáñez <francisco.gonzalez-ibanez@capgemini.com>
2019-09-16 22:02:49 +0200

    Move consuming point to another thread

--- src/main/java/org/fragonib/wormhole/greeting/GreetingController.java
+++ src/main/java/org/fragonib/wormhole/greeting/GreetingController.java
@@ -1,5 +1,10 @@
 package org.fragonib.wormhole.greeting;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,17 +27,28 @@ public class GreetingController {
     @Autowired
     private GreetingService greetingService;
 
+    private final ExecutorService aExecutorService = Executors.newSingleThreadExecutor();
+
     @GetMapping("/greeting")
     public String greets() {
-        putInContextLastVisitUserName();
-        String greeting = greetingService.sayHello();
+        this.putInContextLastVisitUserName();
+        final String greeting = this.runInOtherThread(() -> this.greetingService.sayHello());
         log.info("Returned greeting was [{}]", greeting);
         return greeting;
     }
 
+    private <T> T runInOtherThread(final Callable<T> aCallable) {
+        try {
+            final Future<T> future = this.aExecutorService.submit(aCallable);
+            return future.get();
+        } catch (final Exception e) {
+            return null;
+        }
+    }
+
     private void putInContextLastVisitUserName() {
-        String lastVisitUserName = retrieveLastVisitedUserName();
-        requestMetadata.ofType(GreetingMetadata.class).setUsername(lastVisitUserName);
+        final String lastVisitUserName = this.retrieveLastVisitedUserName();
+        this.requestMetadata.ofType(GreetingMetadata.class).setUsername(lastVisitUserName);
         log.debug("Stored username [{}] into context", lastVisitUserName);
     }
 
